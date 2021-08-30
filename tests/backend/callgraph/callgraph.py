d1645d0db8ea1b9448d8ed242b1d291dc3f30270
dragonslayerintraining <22734948+dragonslayerintraining@users.noreply.github.com>
2021-08-30 15:12:58 -0700

    Refactored callgraph.py to put temporary files in a temporary directory

--- tests/backend/callgraph/callgraph.py
+++ tests/backend/callgraph/callgraph.py
@@ -3,10 +3,13 @@ import igraph
 import subprocess
 import sys
 import os
+import re
+import tempfile
+import shutil
 
 def decode_base64_graph(path):
     g = igraph.Graph().Read_Ncol(path)
-    g.vs["name"]=[base64.b64decode(s).decode('utf-8') for s in g.vs["name"]]
+    g.vs['name']=[base64.b64decode(s).decode('utf-8') for s in g.vs['name']]
     return g
 
 def generate_call_graph(apk_path):
@@ -16,28 +19,28 @@ def generate_call_graph(apk_path):
     print(f"apk={apk}")
     
     subprocess.run(['docker','build','icc','-t','myic3'])
-    subprocess.run(['mkdir','input/'])
-    subprocess.run(['rm','-rf','output/'])
-    subprocess.run(['mkdir','output/'])
-    subprocess.run(['cp',apk_path,'input'])
-    subprocess.run(['docker','run','-it','-v',f'{android_platforms_path}:/srv/android-platforms:ro','-v',f'{os.getcwd()}/input:/srv/input:ro','-v',f'{os.getcwd()}/output:/srv/output','myic3:latest','java','-jar','target/ic3-0.2.1-full.jar','-cp','/srv/android-platforms','-a',f'/srv/input/{apk}','-protobuf','/srv/output'])
+    
+    with tempfile.TemporaryDirectory() as tmpdirname:
+        inputdir = os.path.join(tmpdirname,'input')
+        outputdir = os.path.join(tmpdirname,'output')
+        os.mkdir(inputdir)
+        os.mkdir(outputdir)
+        shutil.copy(apk_path,inputdir)
+        subprocess.run(['docker','run','-it','-v',f'{android_platforms_path}:/srv/android-platforms:ro','-v',f'{inputdir}:/srv/input:ro','-v',f'{outputdir}:/srv/output','myic3:latest','java','-jar','target/ic3-0.2.1-full.jar','-cp','/srv/android-platforms','-a',f'/srv/input/{apk}','-protobuf','/srv/output'])
 
-    assert(len(os.listdir('output'))==1)
+        assert(len(os.listdir(outputdir))==1)
 
-    icc_path = os.path.join('output',os.listdir('output')[0])
+        icc_path = os.path.join(outputdir,os.listdir(outputdir)[0])
                    
-    env = os.environ.copy()
-    env['CLASSPATH']=f".:{os.environ['PWD']}/soot-infoflow-cmd-2.9.0-jar-with-dependencies.jar"
-    subprocess.run(['javac','AndroidCallgraph.java'],env=env)
-    subprocess.run(['java','AndroidCallgraph',apk_path,'basic.lgl'],env=env)
-    subprocess.run(['java','AndroidCallgraph',apk_path,'icc.lgl',icc_path],env=env)
+        env = os.environ.copy()
+        env['CLASSPATH']=f".:{os.environ['PWD']}/soot-infoflow-cmd-2.9.0-jar-with-dependencies.jar"
+        subprocess.run(['javac','AndroidCallgraph.java'],env=env)
 
-#If apk_path is None, use lgl files from last run
-def get_call_graph(apk_path=None):
-    if apk_path:
-        generate_call_graph(apk_path)
-    basic_graph = decode_base64_graph("basic.lgl")
-    graph = decode_base64_graph("icc.lgl")
+        subprocess.run(['java','AndroidCallgraph',apk_path,os.path.join(tmpdirname,'basic.lgl')],env=env)
+        subprocess.run(['java','AndroidCallgraph',apk_path,os.path.join(tmpdirname,'icc.lgl'),icc_path],env=env)
+
+        basic_graph = decode_base64_graph(os.path.join(tmpdirname,'basic.lgl'))
+        graph = decode_base64_graph(os.path.join(tmpdirname,'icc.lgl'))
 
     for e in graph.es:
         e["icc"]=True
@@ -46,17 +49,29 @@ def get_call_graph(apk_path=None):
         target_name = basic_graph.vs['name'][e.target]
         eid = graph.get_eid(source_name,target_name) # may not handle multiple edges as desired
         graph.es[eid]["icc"]=False        
+    
+    for v in graph.vs:
+        m = re.match('^<(.*): (.*) (.*)\(.*\)>$',v['name'])
+        v['class_sig'] = m.group(1)
+        v['class_name'] = v['class_sig'].split('.')[-1]
+        v['return_type'] = m.group(2)
+        v['func_name'] = m.group(3)
+        v['short_name'] = v['class_name']+'.'+v['func_name']
 
     return graph
 
 
-def locate_nodes(g, sigs):
-	return g.subgraph(sigs)
-
 if __name__=='__main__':
     if len(sys.argv)>1:
-        graph = get_call_graph(sys.argv[1])
-    else:
-        graph = get_call_graph()
+        graph = generate_call_graph(sys.argv[1])
+        graph.write_pickle('graph.p')
+
+    graph = igraph.Graph.Read_Pickle('graph.p')
+
+    # graph = graph.subgraph([v.index for v in graph.vs if v['class_name']=='Configs']) # keep only functions from the class Configs
+    
     color_dict = {False:'blue',True:'red'}
-    igraph.plot(graph,vertex_size=0,bbox=(0,0,1000,1000),edge_color=[color_dict[icc] for icc in graph.es["icc"]])
+
+    plot = igraph.plot(graph,vertex_size=0,bbox=(0,0,1000,1000),edge_color=[color_dict[icc] for icc in graph.es["icc"]], vertex_label=graph.vs['short_name'])
+    
+    plot.save('graph.png')
